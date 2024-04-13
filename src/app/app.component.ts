64f72333ecbf615928dbb2e5d161127cbcf13f24
stefanscheffler <sscheffler@avantgarde-labs.de>
2024-04-13 13:42:18 +0200

    add firebase to project / load sample data from firestore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Firestore, getDocs, collection } from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,14 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'sunrise-firebase';
+  firestore = inject(Firestore);
+
+
+
+  ngOnInit() {
+    getDocs(collection(this.firestore, "testCollection")).then((response) => {
+      console.log("response.docs");
+      console.log(response.docs[0].data());
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes),
+    importProvidersFrom(
+      provideFirebaseApp(() =>
+        initializeApp({
+          projectId: 'sunrise-403413',
+          appId: '1:98482988906:web:44ec6de0e1709e689ea713',
+          databaseURL:
+            'https://sunrise-403413-default-rtdb.europe-west1.firebasedatabase.app',
+          storageBucket: 'sunrise-403413.appspot.com',
+          apiKey: 'AIzaSyAURgxyTlCaWV8E011m0SYrP0L5d7fJ2eM',
+          authDomain: 'sunrise-403413.firebaseapp.com',
+          messagingSenderId: '98482988906',
+          measurementId: 'G-18NNE061QV',
+        })
+      )
+    ),
+    importProvidersFrom(provideAuth(() => getAuth())),
+    importProvidersFrom(provideFirestore(() => getFirestore())),
+  ],
 };
